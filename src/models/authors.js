0cc667ab6167bd337b1f1d4bd77709841e6e0875
emanashraf930 <66448860+emanashraf930@users.noreply.github.com>
2022-02-23 17:46:39 +0200

    authors

--- src/models/authors.js
+++ src/models/authors.js
@@ -6,54 +6,84 @@ const validator = require('validator')
     // authers--> age, email, age, phone, password
 const authorsSchema = new mongoose.Schema({
 
-    address: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
+        address: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid')
+                }
             }
-        }
-    },
-    age: {
-        type: Number,
-        default: 20,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be postive number')
+        },
+        age: {
+            type: Number,
+            default: 20,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error('Age must be postive number')
 
+                }
             }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 6,
-        validate(value) {
-            let reg = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])");
-            if (!reg.test(value)) {
-                throw new Error('Password must include uppercase, lowercase,special characer & number')
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minLength: 6,
+            validate(value) {
+                let reg = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])");
+                if (!reg.test(value)) {
+                    throw new Error('Password must include uppercase, lowercase,special characer & number')
+                }
             }
-        }
-    },
-    phone: {
-        type: Number,
-        validate(value) {
+        },
+        phone: {
+            type: Number,
+            validate(value) {
 
-            if (!validator.isMobilePhone(value)) {
-                throw new Error('the number is error .. please try again')
+                if (!validator.isMobilePhone(value)) {
+                    throw new Error('the number is error .. please try again')
+                }
             }
         }
+
+    })
+    // login
+
+authorsSchema.statics.findByCredentials = async(email, password) => {
+    const authors = await Authors.findOne({ email })
+        // console.log(user)
+    if (!authors) {
+        throw new Error('Unable to login..check email or password')
     }
+    // false 
+    const isMatch = await bcrypt.compare(password, authors.password)
+        // console.log(isMatch)
+    if (!isMatch) {
+        throw new Error('Unable to login..check email or password')
+    }
+    return authors
 
-})
+}
+
+
+authorsSchema.methods.generateToken = async function() {
+
+    const authors = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'node-course')
+
+    authors.tokens = authors.tokens.concat(token)
+    await authors.save()
+
+    return token
+}
+
 const Authors = mongoose.model('authors', authorsSchema)
 module.exports = Authors
--- src/routers/authors.js
+++ src/routers/authors.js
@@ -1,26 +1,92 @@
 const express = require('express')
 const Authors = require('../models/authors')
 const router = new express.Router()
-    // get all users
 
+//post
+router.post('/authors', async(req, res) => {
+    try {
+        const authors = new Authors(req.body)
+        await authors.save()
+        const token = await authors.generateToken()
+        res.status(200).send({ authors, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
+//get all
 router.get('/authors', (req, res) => {
-        Authors.find({}).then((authors) => {
-            res.status(200).send(authors)
-        }).catch((e) => {
-            res.status(500).send(e)
-        })
-    })
-    // 
-
-router.post('/authors', (req, res) => {
-
-    const authors = new Authors(req.body)
-    authors.save().then(() => {
+    Authors.find({}).then((authors) => {
         res.status(200).send(authors)
     }).catch((e) => {
-        res.status(400).send(e)
+        res.status(500).send(e)
     })
 })
 
-// get by id
+
+//login
+router.post('/login', async(req, res) => {
+    try {
+        const authors = await Authors.findByCredentials(req.body.email, req.body.password)
+        const token = await Authors.generateToken()
+        res.status(200).send({ authors, token })
+    } catch (e) {
+        res.status(400).send(e.message)
+    }
+})
+
+
+// get profile
+router.get('/profile', async(req, res) => {
+    res.status(200).send(req.authors)
+})
+
+
+//patch
+router.patch('/authors/:id', async(req, res) => {
+    try {
+        const updates = Object.keys(req.body)
+        const authors = await Authors.findById(req.params.id)
+
+        if (!authors) {
+            return res.status(404).send('No user is found')
+        }
+
+        updates.forEach((el) => (authors[el] = req.body[el]))
+        await authors.save()
+        res.status(200).send(authors)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
+//delete profile by id 
+router.delete('/authors/:id', async(req, res) => {
+    try {
+        const authors = await Authors.findByIdAndDelete(req.params.id)
+        if (!authors) {
+            return res.status(404).send('No user is found')
+        }
+        res.status(200).send(authors)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
+//logout
+router.delete('/logout', async(req, res) => {
+    try {
+        req.authors.tokens = req.authors.tokens.filter((el) => {
+            return el !== req.token
+        })
+        await req.authors.save()
+        res.send('Logout Successfully')
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
 module.exports = router
